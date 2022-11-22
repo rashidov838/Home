ed33fda8cd621a398547c85fd7eb1a09018db809
rashidov838 <bekzodrashidov73@gmail.com>
2022-11-22 16:37:58 +0500

    Adds exe2-2

--- semester1/lesson20/h20.py
+++ semester1/lesson20/h20.py
@@ -3,3 +3,12 @@ def sort_by_length(arr):
 
 a=["Telescopes", "Glasses", "Eyes", "Monocles"]
 print(sort_by_length(a))
+def spacey(array):
+    strr = ''
+    out = []
+    for el in array:
+        strr += str(el)
+        out.append(strr)
+    return out
+a=['i', 'have','no','space']
+print(spacey(a))
