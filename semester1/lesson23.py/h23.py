361c8efa2387cfc2d575c9278791c2f2a0642735
rashidov838 <bekzodrashidov73@gmail.com>
2022-11-29 17:26:05 +0500

    Adds a game

--- semester1/lesson23.py/h23.py
+++ semester1/lesson23.py/h23.py
@@ -41,48 +41,76 @@
 
 
 
-# class Personage():
-#     def __init__(self,health,mana,damage,type,skill,name):
-#         self.health=health
-#         self.mana=mana
-#         self.type=type
-#         self.skill=skill
-#         self.damage=damage
-#         self.name=name
-#     def attack(self):
-#         self.attacks=self.health-self.damage
-#         return self.attacks
-#     # def heal(self,heal):
-        
+class Personage():
+    def __init__(self,health,mana,damage,type,skill,name):
+        self.health=health
+        self.mana=mana
+        self.type=type
+        self.skill=skill
+        self.damage=damage
+        self.name=name
+    def attack(self):
+        self.attacks=self.health-self.damage
+        return self.attacks
 
-#     def level_up(self,levelup):
-#         levelup+=10
-#         if self.attacks<=0:
-#             return levelup
+    def heals(self):
+        self.heal=self.health-self.attacks
+        return self.heal
 
-# class Archer(Personage):
-#     def __init__(self,health,mana,damage,type,skill,name):
-#         super().__init__(health,mana,damage,type,skill,name)
+    def level_up(self):
+        self.levelup=0
+        if self.attacks<1000:
+            return self.levelup-10
+        else:
+            return self.levelup+10
 
-#     # def attack():
-#     #     super().attack(self.attack)
+class Archer(Personage):
+    def __init__(self,health,mana,damage,type,skill,name):
+        super().__init__(health,mana,damage,type,skill,name)
 
-# class PAladin(Personage):
-#     def __init__(self,health,mana,damage,type,skill,name):
-#         super().__init__(health,mana,damage,type,skill,name)
+    def attack(self):
+        return super().attack()
+    def level_up(self):
+        return super().level_up()
+    def heals(self):
+        return super().heals()
+
+class PAladin(Personage):
+    def __init__(self,health,mana,damage,type,skill,name):
+        super().__init__(health,mana,damage,type,skill,name)
+
+    def attack(self):
+        return super().attack()
+
+    def level_up(self):
+        return super().level_up()
+    def heals(self):
+        return super().heals()
+
+class Wizard(Personage):
+    def __init__(self,health,mana,damage,type,skill,name):
+        super().__init__(health,mana,damage,type,skill,name)
+
+    def attack(self):
+        return super().attack()
+
+    def level_up(self):
+        return super().level_up()
+    def heals(self):
+        return super().heals()    
+
+personages=Personage(1000,100,150,"Protagonist","Shoot","Persogage")
+personages_1=Archer(1000,200,150,'Warrior','Beat',"Archer")
+personages_2=PAladin(9000,300,150,'Ranger','Kill',"PAladin")
+personages_3=Wizard(800,400,150,'Mage','Destroy',"Wizard")
+# print(personages.health)
+
+print(personages_3.attack(),personages_3.level_up(),personages_3.heals())
+print(personages_2.attack(),personages_2.level_up(),personages_2.heals())
+print(personages_1.attack(),personages_1.level_up(),personages_1.heals())
 
 
-# class Wizard(Personage):
-#     def __init__(self,health,mana,damage,type,skill,name):
-#         super().__init__(health,mana,damage,type,skill,name)
-        
 
-# personages=Personage(1000,100,50,"Protagonist","Shoot","Persongage")
-# personages_1=Archer(1000,200,50,'Protagonist','Shoot',"Archer")
-# personages_2=PAladin(1000,300,50,'Protagonist','Shoot',"PAladin")
-# personages_3=Wizard(1000,400,50,'Protagonist','Shoot',"Wizard")
-# # print(personages.health)
-# print(personages.attack())
 
 # # Codewars
 # class Block:
@@ -109,44 +137,44 @@
 # b = Block([2,4,6])
 # print(b.get_height())
 # print(b.get_surface_area())
-class object:
-    def __init__(self,radius,mass):
-        self.radius=radius
-        self.mass=mass
+# class object:
+#     def __init__(self,radius,mass):
+#         self.radius=radius
+#         self.mass=mass
         
-class Sphere(object):
-    def __init__(self, radius, mass):
-        super().__init__(radius, mass)
-        self.p=3.14          
-    def get_radius(self):
-        if self.radius==2:
-            return f"Check radius {self.radius}"
+# class Sphere(object):
+#     def __init__(self, radius, mass):
+#         super().__init__(radius, mass)
+#         self.p=3.14          
+#     def get_radius(self):
+#         if self.radius==2:
+#             return f"Check radius {self.radius}"
 
-    def get_mass(self):
-        if self.mass==50:
-            return f"Check mass {self.mass}"
+#     def get_mass(self):
+#         if self.mass==50:
+#             return f"Check mass {self.mass}"
 
-    def get_volume(self):
-        self.volume=4/3*self.p*(self.radius**3)
-        if self.volume:
-            return f"Check volume{self.volume}"
+#     def get_volume(self):
+#         self.volume=4/3*self.p*(self.radius**3)
+#         if self.volume:
+#             return f"Check volume{self.volume}"
             
-    def get_surface_area(self):      
-        self.surface_area=4*self.p*self.radius*2
-        if self.surface_area:
-            return f"Check area {self.surface_area}"
-    def get_density(self):
-        self.density=(self.mass/self.volume)
-        if self.density:
-            return f"Check density{self.density}"       
+#     def get_surface_area(self):      
+#         self.surface_area=4*self.p*self.radius*2
+#         if self.surface_area:
+#             return f"Check area {self.surface_area}"
+#     def get_density(self):
+#         self.density=(self.mass/self.volume)
+#         if self.density:
+#             return f"Check density{self.density}"       
 
 
-ball = Sphere(2,50)
-print(ball.get_mass())
-print(ball.get_radius())
-print(ball.get_volume())
-print(ball.get_density())
-print(ball.get_surface_area())
+# ball = Sphere(2,50)
+# print(ball.get_mass())
+# print(ball.get_radius())
+# print(ball.get_volume())
+# print(ball.get_density())
+# print(ball.get_surface_area())
 # https://www.codewars.com/kata/55c1d030da313ed05100005d/train/python
 # test.assert_equals(ball.get_radius(),2, "Check radius")
 # test.assert_equals(ball.get_mass(),50, "Check mass")
