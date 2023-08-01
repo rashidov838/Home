69503c6217b176d4d6c6e1c484075792876b3a93
rashidov838 <bekzodrashidov73@gmail.com>
2023-08-01 14:18:40 +0500

    Update h9.py

--- semester3/lesson-9/h9.py
+++ semester3/lesson-9/h9.py
@@ -25,30 +25,49 @@ class Store:
         self.card_balance=card_balance
         self.connection=sqlite3.connect('store_database.db')
         self.cursor=self.connection.cursor()
-        # self.cursor.execute("""
-        #     CREATE TABLE IF NOT EXISTS information(
-        #         name varchar(200),
-        #         email varchar(50),
-        #         password varchar(100),
-        #         card_code varchar(100),
-        #         card_balance decimal
-        #     )
-        # """)
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS information(
+                id integer primary key autoincrement,
+                name varchar(200),
+                email varchar(50) unique,
+                password varchar(100),
+                card_code varchar(100),
+                card_balance decimal
+            )
+        """)
         
-        # self.cursor.execute("""
-        #     INSERT INTO information(name,email,password,card_code,card_balance) VALUES('Behruz','234asd@gmail.com','12313123','1234567890098765',500)
-        #     """)
+        self.cursor.execute("""
+            INSERT INTO information(name,email,password,card_code,card_balance) VALUES('Behruz','234asd@gmail.com','12313123','1234567890098765',500)
+            """)
 
-        # self.cursor.execute("""
-        #     CREATE TABLE IF NOT EXISTS products(
-        #         name varchar(200),
-        #         price integer
-        #     )
-        # """)
-        # self.cursor.execute("""
-        #     INSERT INTO products VALUES('баклажан',999)
-        #     """)
-        # self.connection.commit()
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS products(
+                id integer primary key autoincrement,
+                name varchar(200) UNIQUE,
+                price integer
+            )
+        """)
+
+        lst_products=[
+            ('wear',200),
+            ('tomato',400),
+            ('kola',300),
+            ('pepsi',500),
+            ('баклажан',800),
+            ('key',783),
+        ]
+        
+        self.cursor.executemany("""INSERT INTO products(name,price) VALUES(?,?)""",lst_products)
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS purchase(
+                id integer primary key autoincrement,
+                product varchar(100) ,
+                price integer references products(price),
+                name varchar(200) references  information(name)
+            )
+        """)
+        self.connection.commit()
         
         
     @classmethod
@@ -56,7 +75,6 @@ class Store:
         connection=sqlite3.connect('store_database.db')
         cursor=connection.cursor()
         for user in cursor.execute('select * from information').fetchall():
-            # print(user)
             if user[1]==email or user[2]==password:
                 return 'Пользователь с такими данными уже есть.'
             else:
@@ -69,8 +87,10 @@ class Store:
             
             information_for_reg.append({  "name": name,"email": email,"password": password,"card_code":card_code,"card_balance":card_balance
 })
-            # cursor.executemany("INSERT INTO information VALUES(:name,:email,:password,:card_code,:card_balance)",tuple(information_for_reg))
-            # connection.commit()
+            cursor.executemany("""
+                INSERT INTO information(name,email,password,card_code,card_balance) VALUES(:name,:email,:password,:card_code,:card_balance)
+            """,tuple(information_for_reg))
+            connection.commit()
             return cls(name,email,password,card_code,card_balance)
         else:
             return 'Wrong credentials'
@@ -82,48 +102,51 @@ class Store:
         if not (email and password):
             return 'Empty values were given'
         for i in cursor.execute('select * from information').fetchall():
-            if i[1]==email and i[2]==password:
-                return cls(i[0],email,password,i[3],i[4])
+            if i[2]==email and i[3]==password:
+                return cls(i[1],email,password,i[4],i[5])
         return 'Wrong email or password'
-    purchases=[]
-    def purchase(self,product):
+    def purchase(self, product):
+        products={}
         connection=sqlite3.connect('store_database.db')
         cursor=connection.cursor()
         for pr in cursor.execute('select * from products').fetchall():
-            if product == pr[0]:
-                return 'Not available'
-            if self.card_balance<=0:
-                return 'Not enough money'
-            
-            if pr[0]==product and self.card_balance-pr[1] >= 0:
-                price=self.card_balance -pr[1]
-                
+            products.update({pr[1]:pr[2]})
+        # print(products)
+
+        if product not in products.keys():
+            return "Not available!"            
+        if self.card_balance <= 0:
+            return "Not enough money!"
+        for key, val in products.items():
+            if key == product and self.card_balance - val >= 0:
+                self.card_balance -= val
                 self.purchases.append(
-                    {'name':product,
-                    'price': price}
+                    {'product':key,
+                    'price':val,
+                    'name':self.name}
                 )
-            cursor.executemany("INSERT INTO products VALUES(:name,:price)",tuple(self.purchases))
-            connection.commit()
 
+        bucket=[]
+        bucket.append(self.purchases[-1])
+        cursor.executemany("""INSERT INTO purchase(product,price,name) VALUES(:product,:price,:name)""",tuple(bucket))
+        connection.commit()
 
-
-enter_method='login'
+enter_method='register'
 if enter_method=='login':
-    user_1=Store.login('234asd@gmail.com','12313123')
+    user_1=Store.login('shax@gmail.com','12fghghs')
     if isinstance(user_1,Store):
-        print(user_1.purchase('wear'))
+        buy_product=['pepsi','tomato','баклажан','key']
+        for i in buy_product:
+            print(user_1.purchase(i))
     else:
         print('Wrong email or password')
 elif enter_method=='register':
-    user_1=Store.register('Bekzod','behruz@gmail.com','21fghghs','4562456245624562',1000)
-    user_2=Store.register('QWERTY','QWERTY@gmail.com','21fghghs','4562956245624562',500)
+    user_1=Store.register('komila','komila@gmail.com','44313123','1234567890098765',99500)
     if isinstance(user_1,Store):
-        print(user_1.purchase('wear'))
+        buy_product=['pepsi','tomato','баклажан','key']
+        for i in buy_product:
+            print(user_1.purchase(i))
     if isinstance(user_1,Store):
-        print(user_1.purchase('key'))
-    else:
+        print(user_1.purchase('pepsi'))
+    else:  
         print('Wrong email or password')
-for user in USERS:
-    print(user)
-print(PRODUCTS)
-
